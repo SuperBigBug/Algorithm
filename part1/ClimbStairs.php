0c6141143b1ffc5ecb857f6dcf058e066986b789
wangzhongyu <wangzhongyu@zhenrongbao.com>
2020-07-08 10:55:52 +0800

    climb stairs

--- part1/ClimbStairs.php
+++ part1/ClimbStairs.php
@@ -19,6 +19,8 @@
 class Solution {
 
     /**
+     * 动态规划
+     *
      * @param Integer $n
      * @return Integer
      */
@@ -34,5 +36,71 @@ class Solution {
             $arr[$i] = $arr[$i-1] + $arr[$i-2];
         }
         return $arr[$n];
+    }
+
+    /**
+     * 暴力法, 存在大量重复计算
+     *
+     * @param $n
+     * @param $i
+     * @return int
+     */
+    function climbStairs1($i, $n) {
+        if ($i>$n) {
+            return 0;
+        }
+        if ($i == $n) {
+            return 1;
+        }
+
+        return $this->climbStairs1($i + 1, $n) + $this->climbStairs1($i + 2, $n);
+    }
+
+    /**
+     * 暴力法记忆优化
+     *
+     * @param $i
+     * @param $n
+     * @param $arr
+     * @return int
+     */
+    function climbStairs2($i, $n, &$arr) {
+        if ($i > $n) {
+            return 0;
+        }
+        if ($i == $n) {
+            return 1;
+        }
+
+        if (isset($arr[$i])) {
+            return $arr[$i];
+        }
+
+        $arr[$i] = $this->climbStairs2($i+1, $n, $arr) + $this->climbStairs2($i+2, $n, $arr);
+
+        return $arr[$i];
+    }
+
+
+    /**
+     * 斐波那契数列
+     *
+     * @param $n
+     * @return int
+     */
+    function climbStairsFib($n) {
+        if ($n == 0) {
+            return 0;
+        }
+
+        $first = 1;
+        $second = 2;
+
+        for ($i=3; $i<=$n; $i++) {
+            $third = $second + $first;
+            $first = $second;
+            $second = $third;
+        }
+        return $second;
     }
 }
