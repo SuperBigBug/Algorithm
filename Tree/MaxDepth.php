a81a890fc4337a7441665b1c4f432868405c605e
wangzhongyu <wangzhongyu@zhenrongbao.com>
2020-07-28 11:47:59 +0800

    二叉树的最大深度

---
+++ Tree/MaxDepth.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhongyu
+ * Date: 2020/7/28
+ * Time: 11:44 AM
+ */
+
+
+class MaxDepth
+{
+    function treeMaxDepth($root)
+    {
+        if ($root == null){
+            return 0;
+        }
+
+        $depth = 1;
+        // 只有一个根节点
+        if ($root->left == null && $root->right == null) {
+            return $depth;
+        }
+
+        $left_max_depth = $this->treeMaxDepth($root->left);
+        $right_max_depth = $this->treeMaxDepth($root->right);
+
+        // 获取最大深度
+        $max_depth = $left_max_depth >= $right_max_depth ? $left_max_depth + $depth : $right_max_depth + $depth;
+
+        return $max_depth;
+    }
+}
