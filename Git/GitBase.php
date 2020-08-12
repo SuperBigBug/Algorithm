c667af0cd3c21e86b9cc45c2fe57b7198d3c3613
wangzhongyu <wangzhongyu@zhenrongbao.com>
2020-08-12 11:53:37 +0800

    git

---
+++ Git/GitBase.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhongyu
+ * Date: 2020/8/11
+ * Time: 7:22 PM
+ */
+
+class GitBase
+{
+    // git add filename 新增单个文件
+    // git add .;       新增所有文件
+    // git commit -m "commit";  提交文件
+    // git status -s 查看工作区状态
+
+    // 拉取远程分支并合并
+    // git fetch branch_name && git merge || git pull branch_name
+
+    // 查看提交记录
+    // git reflog
+
+    // 撤销提交到索引区的文件 git reset HEAD file
+
+    //git stash 、 git stash pop
+
+    //git log -3 查看最近提交的n次记录
+
+    //git fetch git pull, git fetch 只是将代码从远端拉到本地，git pull 将代码从远端拉到本地并合并
+
+    // git diff
+}
