fc6e00c474c70097863533a694b142ebaf0be561
wangzhongyu <wangzhongyu@zhenrongbao.com>
2020-08-13 17:03:23 +0800

    singleton && mysql index

--- DesignPatterns/Singleton.php
+++ DesignPatterns/Singleton.php
@@ -6,6 +6,18 @@
  * Time: 2:48 PM
  */
 
+
+// todo 优、缺点
+
+/**
+ * Class Singleton
+ * 优点: 禁止实例化，确保所有对象都访问唯一实例
+ *      避免大量的new操作, 每一次new操作都要消耗内存资源和系统资源
+ * 缺点: 每次请求时都要检查是否存在类的实例, 需要一些开销
+ *
+ */
+
+
 class Singleton
 {
     // 定义静态私有变量保存类的实例
@@ -30,3 +42,33 @@ class Singleton
         echo 'test';
     }
 }
+
+class StudySingleton
+{
+    static private $instance;
+
+    // 防止使用new直接创建对象
+    private function __construct()
+    {
+    }
+
+    // 防止使用clone克隆对象
+    private function __clone()
+    {
+        // TODO: Implement __clone() method.
+    }
+
+    static public function getInstance()
+    {
+        if (!self::$instance instanceof self){
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    public function test(){
+        return 'hello my friend';
+    }
+}
+
+$res = StudySingleton::getInstance()->test();
---
+++ Redis/RedisBase.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhongyu
+ * Date: 2020/8/13
+ * Time: 4:24 PM
+ */
+
+class RedisBase
+{
+
+}
