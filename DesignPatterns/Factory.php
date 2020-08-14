47afcde78432ea39ca66fbc283167257167c197e
SuperBigBug <942430250@qq.com>
2020-08-14 23:20:49 +0800

    Factory

--- DesignPatterns/Factory.php
+++ DesignPatterns/Factory.php
@@ -6,7 +6,42 @@
  * Time: 2:53 PM
  */
 
-class Factory
-{
+// 工厂模式
 
+// 高级英语翻译 : one word go, jia jia jia    Do you know the meaning?
+
+interface Transports {
+    public function go();
 }
+
+class Bike implements Transports{
+    public function go()
+    {
+        echo 'Let us biking together!';
+    }
+}
+
+class Air implements Transports
+{
+    public function go(){
+        echo 'Let us flying together!';
+    }
+}
+
+class FactoryClass
+{
+    static public function factory($type) {
+        switch ($type) {
+            case 'Air':
+                return new Air();
+                break;
+            case 'Bike':
+                return new Bike();
+                break;
+            default:
+                return new Bike();
+        }
+    }
+}
+
+FactoryClass::factory('Bike')->go();
