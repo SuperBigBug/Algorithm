13c384b52ab8030af1e293ed41ddac2b825b1c12
wangzhongyu <942430250@qq.com>
2020-08-28 15:29:06 +0800

    补充魔术方法 && 命名空间等

---
+++ Http/DNS.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhongyu
+ * Date: 2020/8/27
+ * Time: 11:21 AM
+ */
+
+class DNS
+{
+    /** DNS解析域名过程 */
+}
--- Php/MagicMethod.php
+++ Php/MagicMethod.php
@@ -8,6 +8,19 @@
 
 class MagicMethod
 {
+    /** require include */
+    # require 引入文件如果不存在，程序终止
+    # include 引入文件如果不存在，只是警告，程序继续执行
+
+    /** 命名空间 */
+    # 命名空间主要解决的是名字冲突问题
+    # 在有命名空间的情况下实现自动加载：spl_autoload_register
+    # spl_autoload_register()函数原理，将传入的参数注册到spl_autoload队列中，并移除系统默认的__autoload函数
+    # 一旦访问未定义的类时，调用spl_autoload_register函数就会按顺序调用注册过的所有函数
+    # psr4编码规范要求必须有一个顶级命名空间如app
+    # 顶级域名空间\自域名空间\类名
+
+
     public function __autoload()
     {
         // 自动加载
@@ -23,17 +36,47 @@ class MagicMethod
 
     public function __destruct()
     {
-        // TODO: Implement __destruct() method.
         // 析构方法是对象在销毁之前自动调用的
     }
 
     public function __clone()
     {
-        // TODO: Implement __clone() method.
         // 在克隆一个对象的时候自动调用, 克隆出来的对象还是不是类的一个实例呢？ 可以使用instanceof判断, 返回true or false
     }
 
-    // __get() 当调用一个未声明的属性时，(该属性当前类、父类不存在时)调用该方法，传入参数为属性名
-    // __set() 当给一个未声明的属性赋值时调用该方法，传递值为属性名和值
+    public function __get($name)
+    {
+        // 当访问未定义的属性时自动调用该函数
+    }
 
+    public function __set($name, $value)
+    {
+        // 当给未定义的属性赋值时自动调用该函数
+    }
+
+    public function __isset($name)
+    {
+        // 当在一个未定义的属性上调用isset()函数时自动调用
+    }
+
+    public function __unset($name)
+    {
+        // TODO: Implement __unset() method.
+    }
+}
+
+class TestMagic
+{
+    public $name;
+
+    // __get()魔术方法在访问不存在的属性时被调用
+    public function __get($name)
+    {
+        return '你访问的属性不存在:'.$name;
+    }
+
+    public function __isset($name)
+    {
+        return $name;
+    }
 }
