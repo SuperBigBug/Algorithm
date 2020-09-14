1480d3e437ab455afde3cdd1ca6ca3d124d06a50
SuperBigBug <942430250@qq.com>
2020-09-14 22:08:14 +0800

    change struct

--- DataStructureAndAlgorithm/DataStructureArray.php
+++ DataStructureAndAlgorithm/DataStructureArray.php
@@ -9,21 +9,21 @@
 class DataStructureArray
 {
     /**
-     * ��������: �������ڴ�ռ䡢Ԫ������һ��    ɱ�������: �������
-     * But, �����б�, insert delete��Ҫ�����������ݰ��˹���
+     * 数组特性: 连续的内存空间、元素类型一致    杀手锏特性: 随机访问
+     * But, 有利有弊, insert delete需要做大量的数据搬运工作
      */
 
     /**
-     * �������Ԫ�صĹ��̣�
+     * 数组访问元素的过程：
      */
-    # �������Ϊ�ڴ��ÿ���洢��Ԫ����һ����ַ, ͨ����ַ�������ڴ��е�����, ����Ҫ����ĳ��Ԫ��ʱ���ȼ���Ԫ�ص�ַ, ���ݵ�ַȥ����
-    # a[int 10] �����Ϊ���������һ���������ڴ�, �ڴ����׵�ַΪbaseAddress
+    # 计算机会为内存的每个存储单元分配一个地址, 通过地址来访问内存中的数据, 当需要访问某个元素时会先计算元素地址, 根据地址去访问
+    # a[int 10] 计算机为改数组分配一块连续的内存, 内存块的首地址为baseAddress
     # address[i] = baseAddress + data_type_size * i
-    # ��������±�������ʵ�ʱ�临�Ӷ���O(1)
+    # 数组根据下标随机访问的时间复杂度是O(1)
 
     /**
-     * Ϊʲô������±��0��ʼ������1 ��
+     * 为什么数组的下标从0开始而不是1 ？
      */
-    # ��������Ԫ�ؼ���Ԫ�ص�ַʱΪa[k] = baseAddress + data_type_size * k
-    # ����Ǵ�1��ʼ, a[k] = baseAddress + data_type_size * ( k-1 ) �����һ�μ��ļ���
+    # 访问数组元素计算元素地址时为a[k] = baseAddress + data_type_size * k
+    # 如果是从1开始, a[k] = baseAddress + data_type_size * ( k-1 ) 多进行一次减的计算
 }
