9a660571ded8c902affb5aa7a135f96c59539316
wangzhongyu <942430250@qq.com>
2020-10-10 20:47:06 +0800

    二分法查找的思想, 以及时间时间复杂度分析

--- Sort/HalfFind.php
+++ Sort/HalfFind.php
@@ -9,30 +9,40 @@
 class HalfFind
 {
     /**
-     * @desc 二分查找
+     * @desc 二分法查找 效率老高了 前提: 必须是有序的数组
+     * @desc 二分法时间复杂度为 O(log n)
      *
-     * @param $arr
-     * @param $target
-     * @return bool|float
+     * @param $nums
+     * @param $val
+     * @return float|int
      */
-    public function find($arr, $target)
+    function find($nums, $val)
     {
-        $s = 0;
-        $e = count($arr);
+        if (count($nums) < 1) {
+            return $nums;
+        }
 
-        $m = floor(($s + $e) / 2);
+        $low = 0;
+        $high = count($nums) - 1;
 
-        while($s <= $e) {
-            if ($arr[$m] == $target) {
-                return $m;
-            }
-            if ($arr[$m] > $target) {
-                $e -= 1;
-            }
-            if ($arr[$m] < $target) {
-                $s += 1;
+        // 易错点一: $low <= $high, 而不是<
+        while($low <= $high) {
+
+            // 中间
+            $key = floor($low + ($high - $low) / 2);
+
+            if ($nums[$key] == $val) {
+               return $key;
+            }elseif ($nums[$key] < $val) {
+                // 易错点二: 边界值变更是 key + 1 或是 key - 1
+                $low = $key + 1;
+            }else{
+                $high = $key - 1;
             }
         }
-        return false;
+        return -1;
     }
+
+    /** 在一定程度上效率高于常数级时间复杂度, 常数级别时间复杂度可以使O(10000) */
+    /** 二分法在pow(2, 32)接近42亿个有序元素数组中进行查找, 最多也只进行32次, 牛逼牛逼 */
 }
